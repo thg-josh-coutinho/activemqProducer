0d943418034c81b0392ebc47f7c82a18dadd886a
Joshua Coutinho <josh.coutinho@thehutgroup.com>
2016-11-02 17:01:18 +0000

    Added XML parsing from the activemq

--- src/main/java/com/mycompany/app/Consumer.java
+++ src/main/java/com/mycompany/app/Consumer.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.io.*;
+import com.example.myschema.*;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -7,7 +9,7 @@ import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.TextMessage;
 import javax.jms.Session;
-
+import javax.xml.bind.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 
@@ -36,4 +38,46 @@ public class Consumer {
 
     }
 
+    private static String getStringMessage(MessageConsumer consumer){
+	
+	try {
+	    Message msg = consumer.receive();
+	    if (! (msg instanceof TextMessage)) {
+		throw new RuntimeException("Expected a TextMessage");
+	    }
+	    return ((TextMessage) msg).getText();
+
+	} catch (Exception e) {
+	    return null;
+	}
+    }
+
+    public static void consumeXMLQueue(String queueName){
+	String resultString = null;
+	try {
+	    Connection conn = new ActiveMQConnectionFactory().createConnection();
+	    conn.start();
+	    Session s = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+	    
+	    resultString = getStringMessage(s.createConsumer(s.createQueue(queueName)));
+	} catch (Exception e) {System.out.println(e); System.exit(1);}
+
+	try {
+	   String packageNames = "com.example.myschema";
+	   JAXBContext jaxbContext = JAXBContext.newInstance(packageNames, Class.forName("Consumer").getClassLoader());  
+   
+	   Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();  
+	   Object o = jaxbUnmarshaller.unmarshal(new File(""));  
+          
+	   if(o instanceof NewOrderRequest)
+	       {
+		   NewOrderRequest no = (NewOrderRequest)o;
+		   System.out.println(no.getLink().getHref());
+	       }
+   
+	} catch (Exception e) {
+	    e.printStackTrace();  
+	}
+    }
 }
